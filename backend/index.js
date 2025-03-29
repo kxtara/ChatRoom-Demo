674e031f0423b23d23785a5dd2d5e6fdbfe42f24
kiara hoheb <Kiara Hoheb>
2025-03-29 12:09:42 -0400

    Working on encrypted messaging

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ const { createServer } = require("node:http");
 const { Server } = require("socket.io");
 const { join } = require("node:path");
 const { anonymousNames } = require("./constants");
+const crypto = require("crypto")
+
+const key = crypto.randomBytes(32)
+const iv = crypto.randomBytes(16)
 
 const app = express();
 const server = createServer(app);
@@ -22,6 +26,22 @@ app.use(express.static(join(__dirname, "dist")));
 app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "dist", "index.html"));
 });
+
+
+const encryptMessage = (message) => {
+  const cipher = crypto.createCipheriv("aes-256-cbc",key,iv)
+  let encrypted = cipher.update(message,"utf8","hex");
+  encrypted += cipher.final("hex");
+  return {encrypted, iv : iv.toString("hex")}
+}
+
+const decryptMessage = (encrypted,iv) => {
+  const decipher = crypto.createDecipheriv("aes-256-cbc",key,Buffer.from(iv,"hex"))
+
+  let decrypted = decipher.update(encrypted,"hex","utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
 
 io.on("connection", (socket) => {
 
@@ -59,6 +79,7 @@ io.on("connection", (socket) => {
     const senderInfo = users[socket.id]; // Retrieve the user's info
     if (senderInfo) {
       const { username } = senderInfo;
+      const {encrypted,iv} = encryptMessage(message)
       io.to(room).emit("chat message", {
         sender: socket.id,
         message,
@@ -68,8 +89,14 @@ io.on("connection", (socket) => {
   });
 
   // Handle client disconnect
-  socket.on("disconnect", () => {
+  socket.on("disconnect", (roomName) => {
     console.log("user disconnected");
+    users[socket.id] = { roomName, username };
+    io.to(roomName).emit(
+      "disconnected",
+      `${username} left the room ${roomName}`,
+      username
+    );
     delete users[socket.id];
   });
 });
